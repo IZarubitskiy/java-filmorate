ec940219c7bd7016f370f21230fadd77fbb66df9
Igor.Zarubitskiy <zarubickiy.igor@gmail.com>
2024-11-05 11:10:44 +0200

    Started Filmorate Project

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,93 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.OptionalLong;
+
+@Slf4j
 @RestController
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        try {
+            customFilmValidator(film);
+        } catch (ValidationException e) {
+            log.error("Валидация не пройдена", e);
+        }
+
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.debug("Фильм добавлен.", film);
+        return film;
+    }
+
+    @PutMapping
+    public void updateFilm(@Valid @RequestBody Film film) throws ValidationException {
+        try {
+            customFilmValidator(film);
+        } catch (ValidationException e) {
+            log.error("Валидация не пройдена", e);
+        }
+
+        OptionalLong idFilm = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .filter(id -> film.equals(films.get(id)))
+                .findFirst();
+
+        if (idFilm.isEmpty()) {
+            log.error("Валидация не пройдена", new ValidationException("Фильм, который необходимо обновить, не существует."));
+            throw new ValidationException("Фильм, который необходимо обновить, не существует.");
+        }
+
+        long id = idFilm.getAsLong();
+        films.put(id, film);
+        log.debug("Фильм обновлен.", film);
+
+    }
+
+    @GetMapping
+    public Collection<Film> findAllFilms() {
+        log.trace("Фильмы получены.", films.values());
+        return films.values();
+
+    }
+
+    public void customFilmValidator(Film film) throws ValidationException {
+        if (film.getName().isBlank() ||
+                film.getName() == null) {
+            throw new ValidationException("Фильм без названия");
+        }
+        if (film.getDescription() != null) {
+            if (film.getDescription().length() > 200) {
+                throw new ValidationException("Описание содержит более 200 символов.");
+            }
+        }
+        if (film.getDuration().isNegative()) {
+            throw new ValidationException("Продолжительность фильма отрицательная.");
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.parse("28.12.1895", DateTimeFormatter.ofPattern("dd.MM.yyyy")))) {
+            throw new ValidationException("Выбрана дата до 28 декабря 1895 года.");
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,97 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.OptionalLong;
+
+@Slf4j
+@RestController
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @PostMapping
+    public User addUser(@Valid @RequestBody User user) {
+
+        try {
+            customUserValidator(user);
+        } catch (ValidationException e) {
+            log.error("Валидация не пройдена", e);
+            return null;
+        }
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        log.debug("Пользователь добавлен.", user);
+        return user;
+    }
+
+    @PutMapping
+    public void updateUser(@Valid @RequestBody User user) throws ValidationException {
+
+        try {
+            customUserValidator(user);
+        } catch (ValidationException e) {
+            log.error("Валидация не пройдена", e);
+            return;
+        }
+
+        OptionalLong idUser = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .filter(id -> user.equals(users.get(id)))
+                .findFirst();
+
+        if (idUser.isEmpty()) {
+            log.error("Валидация не пройдена", new ValidationException("Пользователь, которого необходимо обновить, не существует."));
+            throw new ValidationException("Пользователь, которого необходимо обновить, не существует.");
+        }
+
+        long id = idUser.getAsLong();
+        users.put(id, user);
+        log.debug("Пользователь обновлен.", user);
+
+    }
+
+
+    @GetMapping
+    public Collection<User> findAllUserss() {
+        return users.values();
+    }
+
+    public void customUserValidator(User user) throws ValidationException {
+        if (user.getEmail().isBlank() ||
+                user.getEmail() == null) {
+            throw new ValidationException("Пустой email");
+        }
+        if (!user.getEmail().contains("@")) {
+            throw new ValidationException("Email не содержит @.");
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем.");
+        }
+        if (user.getLogin() == null
+                || user.getLogin().isBlank()) {
+            throw new ValidationException("Логин не может быть пустым");
+        }
+        if (user.getLogin().contains(" ")) {
+            throw new ValidationException("Логин не должен содержать пробелы");
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
