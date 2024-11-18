212334d0f8f79bd35305b4ea1f7850721d2132d6
Igor.Zarubitskiy <zarubickiy.igor@gmail.com>
2024-11-18 12:12:29 +0200

    Finalized tests pass

--- src/test/java/ru/yandex/practicum/filmorate/controller/InMemoryUserStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/InMemoryUserStorageTest.java
@@ -1,8 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -10,8 +14,11 @@ import java.time.format.DateTimeFormatter;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class UserControllerTest {
-    UserController userController;
+@SpringBootTest
+public class InMemoryUserStorageTest {
+
+    @Autowired
+    private InMemoryUserStorage inMemoryUserStorage;
 
     User user1 = new User("karamba@bat.com",
             "ElBarto",
@@ -25,15 +32,14 @@ class UserControllerTest {
 
     @Test
     void create() {
-        assertDoesNotThrow(() -> userController.create(user1), "Выкидывает исключение.");
-        assertThrows(ValidationException.class, () -> userController.create(user1), "Не выкидывает исключение.");
+        assertDoesNotThrow(() -> inMemoryUserStorage.create(user1), "Выкидывает исключение.");
+        assertThrows(ValidationException.class, () -> inMemoryUserStorage.create(user1), "Не выкидывает исключение.");
     }
 
     @Test
     void update() {
-        userController.create(user1);
-        assertThrows(ValidationException.class, () -> userController.update(user3), "Не выкидывает исключение.");
-        userController.create(user4);
-        System.out.println(userController.findAllUsers());
+        assertThrows(NotFoundException.class, () -> inMemoryUserStorage.update(user3), "Не выкидывает исключение.");
+        inMemoryUserStorage.create(user4);
+        System.out.println(inMemoryUserStorage.findAll());
     }
 }
